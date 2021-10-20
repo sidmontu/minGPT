0bd2c5c7d68b230ca4a4a17e9ba92b11cc651c1e
Siddhartha <siddhart005@e.ntu.edu.sg>
2021-10-20 18:27:29 +0800

    making it into a poetry project

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -1,20 +1,20 @@
 """
-Simple training loop; Boilerplate that could apply to any arbitrary neural network,
-so nothing in this file really has anything to do with GPT specifically.
+Simple training loop; Boilerplate that could apply to any arbitrary neural
+network, so nothing in this file really has anything to do with GPT
+specifically.
 """
 
-import math
 import logging
+import math
+from typing import Union
 
-from tqdm import tqdm
 import numpy as np
-
 import torch
-import torch.optim as optim
-from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
+
 
 class TrainerConfig:
     # optimization parameters
@@ -23,21 +23,27 @@ class TrainerConfig:
     learning_rate = 3e-4
     betas = (0.9, 0.95)
     grad_norm_clip = 1.0
-    weight_decay = 0.1 # only applied on matmul weights
-    # learning rate decay params: linear warmup followed by cosine decay to 10% of original
+    weight_decay = 0.1  # only applied on matmul weights
+
+    # learning rate decay params: linear warmup followed by cosine decay to 10%
+    # of original
     lr_decay = False
-    warmup_tokens = 375e6 # these two numbers come from the GPT-3 paper, but may not be good defaults elsewhere
-    final_tokens = 260e9 # (at what point we reach 10% of original LR)
+
+    # these two numbers come from the GPT-3 paper, but may not be good defaults
+    # elsewhere
+    warmup_tokens = 375e6
+    final_tokens = 260e9  # (at what point we reach 10% of original LR)
+
     # checkpoint settings
     ckpt_path = None
-    num_workers = 0 # for DataLoader
+    num_workers = 0  # for DataLoader
 
     def __init__(self, **kwargs):
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             setattr(self, k, v)
 
-class Trainer:
 
+class Trainer:
     def __init__(self, model, train_dataset, test_dataset, config):
         self.model = model
         self.train_dataset = train_dataset
@@ -45,7 +51,7 @@ class Trainer:
         self.config = config
 
         # take over whatever gpus are on the system
-        self.device = 'cpu'
+        self.device: Union[int, str] = "cpu"
         if torch.cuda.is_available():
             self.device = torch.cuda.current_device()
             self.model = torch.nn.DataParallel(self.model).to(self.device)
@@ -61,16 +67,24 @@ class Trainer:
         raw_model = model.module if hasattr(self.model, "module") else model
         optimizer = raw_model.configure_optimizers(config)
 
-        def run_epoch(split):
-            is_train = split == 'train'
+        def run_epoch(split, epoch):
+            is_train = split == "train"
             model.train(is_train)
             data = self.train_dataset if is_train else self.test_dataset
-            loader = DataLoader(data, shuffle=True, pin_memory=True,
-                                batch_size=config.batch_size,
-                                num_workers=config.num_workers)
+            loader = DataLoader(
+                data,
+                shuffle=True,
+                pin_memory=True,
+                batch_size=config.batch_size,
+                num_workers=config.num_workers,
+            )
 
             losses = []
-            pbar = tqdm(enumerate(loader), total=len(loader)) if is_train else enumerate(loader)
+            pbar = (
+                tqdm(enumerate(loader), total=len(loader))
+                if is_train
+                else enumerate(loader)
+            )
             for it, (x, y) in pbar:
 
                 # place data on the correct device
@@ -80,7 +94,9 @@ class Trainer:
                 # forward the model
                 with torch.set_grad_enabled(is_train):
                     logits, loss = model(x, y)
-                    loss = loss.mean() # collapse all losses if they are scattered on multiple gpus
+                    loss = (
+                        loss.mean()
+                    )  # collapse all losses if they are scattered on multiple gpus
                     losses.append(loss.item())
 
                 if is_train:
@@ -88,42 +104,57 @@ class Trainer:
                     # backprop and update the parameters
                     model.zero_grad()
                     loss.backward()
-                    torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
+                    torch.nn.utils.clip_grad_norm_(
+                        model.parameters(), config.grad_norm_clip
+                    )
                     optimizer.step()
 
                     # decay the learning rate based on our progress
                     if config.lr_decay:
-                        self.tokens += (y >= 0).sum() # number of tokens processed this step (i.e. label is not -100)
+                        self.tokens += (
+                            y >= 0
+                        ).sum()  # number of tokens processed this step (i.e. label is not -100)
                         if self.tokens < config.warmup_tokens:
                             # linear warmup
-                            lr_mult = float(self.tokens) / float(max(1, config.warmup_tokens))
+                            lr_mult = float(self.tokens) / float(
+                                max(1, config.warmup_tokens)
+                            )
                         else:
                             # cosine learning rate decay
-                            progress = float(self.tokens - config.warmup_tokens) / float(max(1, config.final_tokens - config.warmup_tokens))
-                            lr_mult = max(0.1, 0.5 * (1.0 + math.cos(math.pi * progress)))
+                            progress = float(
+                                self.tokens - config.warmup_tokens
+                            ) / float(
+                                max(1, config.final_tokens - config.warmup_tokens)
+                            )
+                            lr_mult = max(
+                                0.1, 0.5 * (1.0 + math.cos(math.pi * progress))
+                            )
                         lr = config.learning_rate * lr_mult
                         for param_group in optimizer.param_groups:
-                            param_group['lr'] = lr
+                            param_group["lr"] = lr
                     else:
                         lr = config.learning_rate
 
                     # report progress
-                    pbar.set_description(f"epoch {epoch+1} iter {it}: train loss {loss.item():.5f}. lr {lr:e}")
+                    pbar.set_description(
+                        f"epoch {epoch+1} iter {it}: train loss {loss.item():.5f}. lr {lr:e}"
+                    )
 
             if not is_train:
                 test_loss = float(np.mean(losses))
                 logger.info("test loss: %f", test_loss)
                 return test_loss
 
-        best_loss = float('inf')
-        self.tokens = 0 # counter used for learning rate decay
+        best_loss = float("inf")
+        self.tokens = 0  # counter used for learning rate decay
         for epoch in range(config.max_epochs):
 
-            run_epoch('train')
+            run_epoch("train", epoch)
             if self.test_dataset is not None:
-                test_loss = run_epoch('test')
+                test_loss = run_epoch("test", epoch)
 
-            # supports early stopping based on the test loss, or just save always if no test set is provided
+            # supports early stopping based on the test loss, or just save
+            # always if no test set is provided
             good_model = self.test_dataset is None or test_loss < best_loss
             if self.config.ckpt_path is not None and good_model:
                 best_loss = test_loss
--- mingpt/utils.py
+++ mingpt/utils.py
@@ -1,8 +1,9 @@
 import random
+
 import numpy as np
 import torch
-import torch.nn as nn
 from torch.nn import functional as F
+
 
 def set_seed(seed):
     random.seed(seed)
@@ -10,24 +11,29 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+
 def top_k_logits(logits, k):
     v, ix = torch.topk(logits, k)
     out = logits.clone()
-    out[out < v[:, [-1]]] = -float('Inf')
+    out[out < v[:, [-1]]] = -float("Inf")
     return out
+
 
 @torch.no_grad()
 def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
     """
-    take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
-    the sequence, feeding the predictions back into the model each time. Clearly the sampling
-    has quadratic complexity unlike an RNN that is only linear, and has a finite context window
-    of block_size, unlike an RNN that has an infinite context window.
+    Take a conditioning sequence of indices in x (of shape (b,t)) and predict
+    the next token in the sequence, feeding the predictions back into the model
+    each time. Clearly the sampling has quadratic complexity unlike an RNN that
+    is only linear, and has a finite context window of block_size, unlike an
+    RNN that has an infinite context window.
     """
     block_size = model.get_block_size()
     model.eval()
-    for k in range(steps):
-        x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
+    for _k in range(steps):
+        x_cond = (
+            x if x.size(1) <= block_size else x[:, -block_size:]
+        )  # crop context if needed
         logits, _ = model(x_cond)
         # pluck the logits at the final step and scale by temperature
         logits = logits[:, -1, :] / temperature
